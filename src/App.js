3a160651368bbebd41ad67bbe2aee4be36724302
Ankita <chaudharyankita755@gmail.com>
2023-03-24 20:14:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import colorArray from "./colorsArray";
+
+let quoteDBurl =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 function App() {
+  const [quote, setQuote] = useState(
+    "'It is never too late to be what you might have been.'"
+  );
+  const [author, setAuthor] = useState("George Eliot");
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [quotesArray, setquotesArray] = useState(null);
+  const [accentColor, setaccentColor] = useState("#282c34");
+
+  const fetchquotes = async (url) => {
+    const response = await fetch(url);
+    const parsedJson = await response.json();
+    setquotesArray(parsedJson.quotes);
+  };
+  useEffect(() => {
+    fetchquotes(quoteDBurl);
+  }, [quoteDBurl]);
+
+  const generateRandomNumber = () => {
+    let randomInteger = Math.floor(quotesArray.length * Math.random());
+    setRandomNumber(randomInteger);
+    setaccentColor(colorArray[randomInteger]);
+    setQuote(quotesArray[randomInteger].quote);
+    setAuthor(quotesArray[randomInteger].author);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ backgroundColor: accentColor }}>
+        <div id="quote-box" style={{ color: accentColor }}>
+          <h1>Random Number : {randomNumber}</h1>
+
+          <p id="text">{quote}</p>
+          <p id="author"> - {author}</p>
+          <div>
+            <a
+              id="tweet-quote"
+              style={{ color: accentColor }}
+              href={`https://twitter.com/`}
+            >
+              Tweet quote
+            </a>
+          </div>
+          <button id="new-quote"  style={{ backgroundColor: accentColor }} onClick={() => generateRandomNumber()}>
+            Generate a random quote
+          </button>
+        </div>
       </header>
     </div>
   );
